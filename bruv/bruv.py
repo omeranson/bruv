9a87edb70679a88267815d030ff5fed0e116e7dc
Shachar Snapiri <shachar.snapiri@huawei.com>
2021-10-10 12:03:23 +0300

    Modify the preparation of the diff URL
    There is no need to build the patchset diff string. Using the base URL
    with the "<old_ps>..<new_ps>" works well

--- bruv/bruv.py
+++ bruv/bruv.py
@@ -181,8 +181,8 @@ def add_last_checked_information(change):
         change["change_since_last_comment"] = (
             current_path_set != last_patch_set)
         if last_patch_set != current_path_set:
-            change["diff_url"] = "http://%s/#/c/%s/%d..%d" % (
-                host, change["number"], last_patch_set, current_path_set
+            change["diff_url"] = "%s/%d..%d" % (
+                change["url"], last_patch_set, current_path_set
             )
     else:
         change["change_since_last_comment"] = True
